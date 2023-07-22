4b82da96d8456b309ec7099e6809305c861119cb
mototvtv55 <139687798+mototvtv55@users.noreply.github.com>
2023-07-22 23:19:29 +0530

    Create start_container.sh

---
+++ scripts/start_container.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+
+# Pull the Docker image from Docker Hub
+docker pull awsdockertest1989/jenkins
+
+# Run the Docker image as a container
+docker run -p 8080:8080 --name=jenkins-master -d jenkins/jenkins
